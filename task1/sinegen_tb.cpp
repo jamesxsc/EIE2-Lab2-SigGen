9d51066e53bfaf8115428e7812dd82259d760b85
jamesxsc <james@xsc.co.uk>
2024-10-22 20:26:59 +0100

    task1 finished

--- task1/sinegen_tb.cpp
+++ task1/sinegen_tb.cpp
@@ -1,8 +1,10 @@
 #include "Vsinegen.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env) {
+    vbdOpen();
     Verilated::commandArgs(argc, argv);
     int i, clk;
     Vsinegen dut;
@@ -18,18 +20,20 @@ int main(int argc, char **argv, char **env) {
     dut.rst = 0;
 
     // loop
-    for (i = 0; i < 5000; i++) {
+    for (i = 0; i < 1000000; i++) {
         for (clk = 0; clk < 2; clk++) {
             trace.dump(2 * i * clk); // TODO check this index - seems non monotonic
             dut.clk = !clk;
             dut.eval();
         }
         // inputs if required (or outputs)
+        vbdPlot(dut.data, 0, 255);
 
-        if (Verilated::gotFinish()) break; // better than exiting to ensure trace is closed
+        if (Verilated::gotFinish() || vbdGetkey() == 'q') break; // better than exiting to ensure trace is closed
     }
 
     trace.close();
+    vbdClose();
     exit(0);
 }
 
