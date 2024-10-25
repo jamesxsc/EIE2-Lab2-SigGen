c2bf8013159561512e854600d999bd7ef630f9f3
eliott <eliott.chestnut@gmail.com>
2024-10-25 12:22:05 +0200

    the rat is with ryan in the kitchen

--- task2/rom.sv
+++ task2/rom.sv
@@ -1,4 +1,8 @@
 module rom (
+
+  input logic clk,
+  input logic en1,                         // Enable for port 1
+  input logic en2,                         // Enable for port 2
   input logic [7:0] addr1,  // First address input
   input logic [7:0] addr2,  // Second address input
   output logic [7:0] data1, // Output data for first port
@@ -11,9 +15,18 @@ module rom (
   initial begin
     $readmemh("sinerom.mem", rom_data);
   end
+    // Output data1 for port 1 on clock edge if en1 is asserted
+    always_ff @(posedge clk) begin
+        if (en1)
+            data1 <= rom_data[addr1];
+    end
 
-  // Assign ROM values to outputs based on addresses
-  assign data1 = rom_data[addr1];
-  assign data2 = rom_data[addr2];
+    // Output data2 for port 2 on clock edge if en2 is asserted
+    always_ff @(posedge clk) begin
+        if (en2)
+            data2 <= rom_data[addr2];
+    end
+
+
 
 endmodule
